ac1878ced9d5e52f7736cb8c0935d728d31e0eaf
cowtipping <turbogerbil@gmail.com>
2023-05-03 14:51:21 +0100

    fixed your stuff

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,24 +2,24 @@ import { useState } from "react";
 import "./style.css";
 
 function List({ names }) {
-  console.log("re-rendered");
-  const [isHappy, setIsHappy] = useState();
+  // changed the state name just to stop me being confused with the object key isHappy
+  const [happiness, setHappiness] = useState(names); // pass in the names array as the initial state
 
-  function handleClick(person) {
-    console.log(person.isHappy, person.name);
-    person.isHappy = !person.isHappy;
-    setIsHappy(person.isHappy);
+  function handleClick(name) {
+    name.isHappy = !name.isHappy; // toggle the isHappy boolean
+    setHappiness([...happiness]); // the spread operator returns a new array so the state knows it needs to update
   }
 
   return (
     <div>
-      {names.map((name) => (
-        <div className="border-me" key={Math.random()}>
+      { /* map through the state array */}
+      {happiness.map((name) => (
+        <div className="border-me" key={Math.random()}> { /* you should provide id keys in your data model instead of using Math.random */}
           <div className="person-div">
             <div
-              {...(name.isHappy
+              {...name.isHappy
                 ? { style: { color: "#eeee00" } }
-                : { style: { color: "#cc0000" } })}
+                : { style: { color: "#cc0000" } }}
             >
               {name.name + " " + (name.isHappy ? "is happy" : "is not happy")}
             </div>
